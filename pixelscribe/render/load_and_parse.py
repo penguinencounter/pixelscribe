5ca5800494a8f09bb5a94419040e188a338933bb
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-03-29 21:31:47 +0000

    Preprocess <pre><code> to <codeblock>

--- pixelscribe/render/load_and_parse.py
+++ pixelscribe/render/load_and_parse.py
@@ -1,3 +1,6 @@
+import copy
+import os
+
 import cmarkgfm  # type: ignore[reportMissingTypeStubs]
 from bs4 import BeautifulSoup
 
@@ -7,9 +10,34 @@ def _gfm_to_html(markdown: str, options: int = 0) -> str:
     return cmarkgfm.github_flavored_markdown_to_html(markdown, options)  # type:ignore
 
 
-def render_markdown() -> BeautifulSoup:
+def render_markdown(source: str) -> BeautifulSoup:
     """Render markdown to html"""
-    with open("README.md", "r") as f:
-        markdown = f.read()
-    html = _gfm_to_html(markdown)
-    return BeautifulSoup(html, "html.parser")
+    html = _gfm_to_html(source)
+    souped = BeautifulSoup(html, "html.parser")
+    ## Preprocessing
+    # swap <pre><code> ... </pre></code> for <codeblock> ... </codeblock> keeping attributes
+    for pre in souped.find_all("pre"):
+        code = pre.find("code")
+        if code is None:
+            continue
+        attrset = copy.deepcopy(code.attrs)
+        attrset.update(pre.attrs)
+        codeblock = souped.new_tag("codeblock", **attrset)  # type: ignore[reportUnknownArgumentType]
+        codeblock.string = code.string
+        pre.replace_with(codeblock)
+    return souped
+
+
+if __name__ == "__main__":
+    source = ""
+    console_marker = "md> " if os.isatty(0) else "."
+    if not os.isatty(0):
+        print("Reading", end="", flush=True)
+    while True:
+        source_line = input(console_marker)
+        if source_line == "$$end":
+            break
+        source += source_line + "\n"
+    soup = render_markdown(source)
+    print()
+    print(soup.prettify())
